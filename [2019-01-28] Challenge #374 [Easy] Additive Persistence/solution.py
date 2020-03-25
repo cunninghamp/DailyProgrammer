348cd3c80382397656110a29336026d432e2b192
Paul Cunningham <paul@locklan.com.au>
2020-03-25 15:53:40 +1000

    2020-03-09 easy challenge

--- [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
+++ [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
@@ -5,21 +5,31 @@ loopcount = 0
 
 def calculate_additive_persistence(inputnumber):
 
+    # Using global variable so that it doesn't reset to 0 on each recursive loop
     global loopcount
     sumofnumbers = 0
 
     # Creates a list object containing the individual digits of the input number
     digits = map(int, (str(inputnumber)))
 
+    # Calculate the sum of the individual digits
     for i in digits:
         sumofnumbers = sumofnumbers + i
     print("Sum of numbers is: " + str(sumofnumbers))
+
+    # Increment the loop count
     loopcount += 1
-    if len(str(sumofnumbers)) > 1:
+
+    # If the sum of the individual digits is greater than 9 (i.e. more than 1 digit
+    # long), do a recursive loop of the function
+    if sumofnumbers > 9:
         calculate_additive_persistence(sumofnumbers)
+
+    # Finally, return the number of times the function ran (looped)
     return loopcount
 
 
+# If the input number is less than 10 the answer is 0, no loops required
 if number < 10:
     answer = 0
 else:
