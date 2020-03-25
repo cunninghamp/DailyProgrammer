67831d465d11bedc43440ce3e0554c56fd7e81a5
Paul Cunningham <paul@locklan.com.au>
2020-03-25 15:50:25 +1000

    2020-03-09 easy challenge

--- [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
+++ [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
@@ -3,12 +3,12 @@ number = int(input("Enter a number: "))
 loopcount = 0
 
 
-def makemap(inputnumber):
+def calculate_additive_persistence(inputnumber):
 
     global loopcount
-
     sumofnumbers = 0
 
+    # Creates a list object containing the individual digits of the input number
     digits = map(int, (str(inputnumber)))
 
     for i in digits:
@@ -16,12 +16,13 @@ def makemap(inputnumber):
     print("Sum of numbers is: " + str(sumofnumbers))
     loopcount += 1
     if len(str(sumofnumbers)) > 1:
-        makemap(sumofnumbers)
+        calculate_additive_persistence(sumofnumbers)
     return loopcount
 
 
-if len(str(number)) == 1:
-    print(loopcount)
+if number < 10:
+    answer = 0
 else:
-    answer = makemap(number)
-    print("Loops required: " + str(answer))
+    answer = calculate_additive_persistence(number)
+
+print("Loops required: " + str(answer))
