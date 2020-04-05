6682f06ba4cfbab7926e531bf60634ac29b2421c
Paul Cunningham <paul@locklan.com.au>
2020-04-05 14:51:34 +1000

    Fixing my bad decision on the last merge

--- [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
+++ [2019-01-28] Challenge #374 [Easy] Additive Persistence/solution.py
@@ -1,27 +1,39 @@
+# Ask the user for a number
 number = int(input("Enter a number: "))
 
 loopcount = 0
 
 
-def makemap(inputnumber):
+def calculate_additive_persistence(inputnumber):
 
+    # Using global variable so that it doesn't reset to 0 on each recursive loop
     global loopcount
-
     sumofnumbers = 0
 
+    # Creates a list object containing the individual digits of the input number
     digits = map(int, (str(inputnumber)))
 
+    # Calculate the sum of the individual digits
     for i in digits:
         sumofnumbers = sumofnumbers + i
-    print(f"Sum of numbers is: {sumofnumbers}")
+    print("Sum of numbers is: " + str(sumofnumbers))
+
+    # Increment the loop count
     loopcount += 1
-    if len(str(sumofnumbers)) > 1:
-        makemap(sumofnumbers)
+
+    # If the sum of the individual digits is greater than 9 (i.e. more than 1 digit
+    # long), do a recursive loop of the function
+    if sumofnumbers > 9:
+        calculate_additive_persistence(sumofnumbers)
+
+    # Finally, return the number of times the function ran (looped)
     return loopcount
 
 
-if len(str(number)) == 1:
-    print(loopcount)
+# If the input number is less than 10 the answer is 0, no loops required
+if number < 10:
+    answer = 0
 else:
-    answer = makemap(number)
-    print(f"Loops required: {answer}")
+    answer = calculate_additive_persistence(number)
+
+print("Loops required: " + str(answer))
